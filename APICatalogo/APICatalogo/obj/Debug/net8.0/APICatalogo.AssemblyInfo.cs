0715e484ac4d609eac818dd0c94570c709fd6d25
Henrique Pyramo <hpyramo92@gmail.com>
2024-07-24 20:36:50 -0300

    Finalizado aula 26

--- APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59944a954f2fa30255c4b58bbdbe8718dc9afea3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9da3ab97fcfb2f69cb2f381f8420f74698f9dcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
